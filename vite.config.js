ea39ad430096efae05f2c2ad6dc9b79384d0cff8
12227990 <e12227990@student.tuwien.ac.at>
2025-08-18 10:45:02 +0200

    some stuff for deployment to host the single page-site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calculator/', // must match your repo name
   plugins: [react()],
+  base: '/calculator/', // needed for GitHub Pages
 })
